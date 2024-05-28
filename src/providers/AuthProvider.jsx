9ac3e3564a0db19226e6fba325edf9b368a7c796
ahmaddwiki <114077731+ahmaddwiki@users.noreply.github.com>
2024-05-28 20:26:19 +0700

    adada

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,7 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }) => {
-  return <SessionProvider basePath="/api/auth">{children}</SessionProvider>;
+  return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default AuthProvider;
